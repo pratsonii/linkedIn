3fc172cd00f50f7591e134e80713afa1ba782ab8
pratiksoni <pratiksonii@yahoo.in>
2020-05-12 10:43:59 +0530

    Username changes

--- src/app/coponents/profile/user-primary-info/user.service.ts
+++ src/app/coponents/profile/user-primary-info/user.service.ts
@@ -9,8 +9,8 @@ export class UserService {
 
   getUserDetails(): Observable<user> {
     return of({
-      name: "Dmitry Kargaev",
-      location: "Saint Petersburg, Russian Federation",
+      name: "Dmitry Daruwala",
+      location: "Melborne, United States",
       bio:
         "Freelance UX/UI designer, 80+ projects in web design, mobile apps (iOS & android) and creative projects. Open to offers.",
     });
